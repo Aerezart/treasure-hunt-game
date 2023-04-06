94b00a2d5de4180da1ee2be612c8da108219eb12
Aerezart <aerezart@gmail.com>
2023-04-06 20:23:51 +0200

    Hotfix

--- src/store/gameInventory.js
+++ src/store/gameInventory.js
@@ -3,18 +3,18 @@ export const allGameItems = [
     id: 1,
     type: "weapon",
     label: "Sabre",
-    image: "../../public/images/sabre.png",
+    image: "images/sabre.png",
   },
   {
     id: 2,
     type: "weapon",
     label: "Coconut",
-    image: "../../public/images/coconut.png",
+    image: "images/coconut.png",
   },
   {
     id: 3,
     type: "weapon",
     label: "Pistol",
-    image: "../../public/images/pistol.png",
+    image: "images/pistol.png",
   },
 ];
