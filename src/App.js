40631ad91d5b24583e71caea807083e1b0bd9dac
Aerezart <aerezart@gmail.com>
2023-04-08 15:30:50 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { useLayoutEffect, useState } from "react";
 import "./App.css";
 import Board from "./Board/Board";
 import ControlsNav from "./ControlsNav/ControlsNav";
 
 function App() {
+
+  const [windoWidth, setWindoWidth] = useState(window.innerWidth)
+
+  
+  useLayoutEffect(() => {
+    setWindoWidth(window.innerWidth)
+    function updateSize() {
+      setWindoWidth(window.innerWidth)
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+  }, [window.innerWidth]);
+
+
+  if(windoWidth<800){
+    return (
+      <div className="app_mobile">
+          <h1>This game is not yet avilable on mobile</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="app">
       <Board />
