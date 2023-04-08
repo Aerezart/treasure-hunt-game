a8bff62c2e56e6643dc6724e919f5ab0944d7b05
Aerezart <aerezart@gmail.com>
2023-04-08 15:08:37 +0200

    Hotfix

--- src/Board/GameFinished.js
+++ src/Board/GameFinished.js
@@ -8,7 +8,7 @@ const GameFinished = ({restartHandler}) => {
             <h1>Congradulations!</h1>
             <h1>You've found it</h1>
             <div className={classes.treasure}>
-            <img src='/images/treasure.png' alt="" />
+            <img src='images/treasure.png' alt="" />
             </div>
             <button id='restart' onClick={restartHandler} className={`${classes.restart} pixel_corners_smaller `}>Restart the Game</button>
         </div>
